a50b80a387146c7157421e58f4ffc477e672c15d
TheGrimbleGrombler <parinifor@gmail.com>
2026-08-31 08:20:57 -0400

    h

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ function updateVisuals() {
 
     Tick = Tick + 0.25;
     document.getElementById("ring1").style.transform = "rotate(" + String(Tick) + "deg)"
-    document.getElementById("ring2").style.transform = "rotate(" + String(Tick) + "deg)"
-    document.getElementById("ring3").style.transform = "rotate(" + String(Tick) + "deg)"
-    document.getElementById("ring4").style.transform = "rotate(" + String(Tick) + "deg)"
+    document.getElementById("ring2").style.transform = "rotate(" + String(Tick + 22.5) + "deg)"
+    document.getElementById("ring3").style.transform = "rotate(" + String(Tick + 45) + "deg)"
+    document.getElementById("ring4").style.transform = "rotate(" + String(Tick + 67.5) + "deg)"
 
     if (Tick >= 100000) {
 
