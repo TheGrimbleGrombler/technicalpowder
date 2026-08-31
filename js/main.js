5db5a3ed5e75b7253c5573fd9b73ea7dcbf3148b
TheGrimbleGrombler <parinifor@gmail.com>
2026-08-31 09:15:25 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 
 var Tick = 0;
-var curTab = "Main"
+var curTab = "Main";
 
 function swapTab(name) {
 
-    var a = document.getElementById("Page_" + curTab)
-    var b = document.getElementById("Page_" + name)
+    var a = document.getElementById("Page_" + curTab);
+    var b = document.getElementById("Page_" + name);
 
     if (a != undefined && b != undefined) {
 
-        a.style.display = "none"
-        b.style.display = "block"
+        a.style.display = "none";
+        b.style.display = "block";
 
-        curTab = name
+        curTab = name;
 
     } else {
 
         if (a == undefined) {
-            console.log("Page " + curTab + " not defined")
+            console.log("Page " + curTab + " not defined");
         } else {
-            console.log("Page " + name + " not defined")
+            console.log("Page " + name + " not defined");
         }
 
     }
@@ -28,12 +28,12 @@ function swapTab(name) {
 
 function setupTabButtons() {
 
-    var buttons = document.getElementsByClassName("pagebutton")
+    var buttons = document.getElementsByClassName("pagebutton");
     for (let i = 0; i < buttons.length; i++) {
 
         buttons[i].addEventListener('click', () => {
             console.log(buttons[i].textContent);
-            swapTab(buttons[i].textContent)
+            swapTab(buttons[i].textContent);
         });
 
     }
@@ -43,10 +43,10 @@ function setupTabButtons() {
 function updateVisuals() {
 
     Tick = Tick + 0.25;
-    document.getElementById("ring1").style.transform = "rotate(" + String(Tick) + "deg)"
-    document.getElementById("ring2").style.transform = "rotate(" + String(Tick + 22.5) + "deg)"
-    document.getElementById("ring3").style.transform = "rotate(" + String(Tick + 45) + "deg)"
-    document.getElementById("ring4").style.transform = "rotate(" + String(Tick + 67.5) + "deg)"
+    document.getElementById("ring1").style.transform = "rotate(" + String(Tick) + "deg)";
+    document.getElementById("ring2").style.transform = "rotate(" + String(Tick + 22.5) + "deg)";
+    document.getElementById("ring3").style.transform = "rotate(" + String(Tick + 45) + "deg)";
+    document.getElementById("ring4").style.transform = "rotate(" + String(Tick + 67.5) + "deg)";
 
     if (Tick >= 100000) {
 
@@ -58,5 +58,5 @@ function updateVisuals() {
 
 document.addEventListener("DOMContentLoaded", function() {
   setInterval(updateVisuals, 16);
-  setupTabButtons()
+  setupTabButtons();
 });
