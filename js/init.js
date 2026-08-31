23915d9838beb76254dc775edec326fa4e40287c
TheGrimbleGrombler <parinifor@gmail.com>
2026-08-31 00:03:27 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -6,6 +6,11 @@ function updateVisuals() {
     Tick = Tick + 1;
     document.getElementById("ring1").style.transform = "rotate(" + String(Tick) + "deg)"
 
+    if (Tick >= 100000) {
+
+        Tick = 1;
+
+    }
 }
 
 
